d96a94d3cbd4b09b8c3ef849aef1ad5e36f751ea
Isabella Costabile <costabileisa@gmail.com>
2023-05-04 13:17:34 -0400

    add files for Dog model and controller

--- client/src/components/Dogs.js
+++ client/src/components/Dogs.js
@@ -1,7 +1,12 @@
 import React from "react"
 
 function Dogs() {
-  
+  return (
+    <div className="available-dogs">
+      <h1>Available Dogs</h1>
+      
+    </div>
+  )
 }
 
 export default Dogs
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+require 'httparty'
+
+response = HTTParty.get('https://api.thedogapi.com/v1/images/search?limit=20')
+dogs = JSON.parse(response.body)
+
+dogs.each do |dog|
+  Dog.create!(
+    breed: dog['breeds'][0]['name'],
+    image_url: dog['url']
+  )
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
