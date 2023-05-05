b2f596e157111a51984abfa5a83c226430cc50f9
Isabella Costabile <costabileisa@gmail.com>
2023-05-05 06:27:18 -0400

    :)

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,5 +1,5 @@
 class DogsController < ApplicationController
-  before_action :authenticate_user, only: [:index, :show]
+  before_action :authenticate_user, except: [:index, :show]
 
   def index
     render json: Dog.all
